4aef4fbc1ccd72a78c5ca7d77573218fdb21439f
Anon SEC 93 <anonsec93@gmail.com>
2026-06-04 09:37:16 +0000

    Add __init__.py for tasks in edgemmeval

---
+++ edgemmeval/edgemmeval/tasks/__init__.py
@@ -0,0 +1,15 @@
+from .image_classification import ImageClassificationTask
+from .asr import ASRTask
+from .object_counting import ObjectCountingTask
+from .action_recognition import ActionRecognitionTask
+from .hazard_detection import HazardDetectionTask
+from .ocr import OCRTask
+
+__all__ = [
+    "ImageClassificationTask",
+    "ASRTask",
+    "ObjectCountingTask",
+    "ActionRecognitionTask",
+    "HazardDetectionTask",
+    "OCRTask",
+]
